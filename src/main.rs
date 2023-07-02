5120977b65f012a81b373e2ab62c4055015a98c0
NaMorham <namorham@gmail.com>
2023-07-03 00:00:59 +1000

    RSLN-0002 Add rand crate and experiment with ansi_test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use std::io::{self,Write};
+use ansi_term::Colour::RGB;
+//use ansi_term::Style;
 
 fn main() {
-    println!("Guss the number!\n");
+	let err = RGB(248, 24, 24).normal();
+	let warn = RGB(230, 126, 34).normal();
+	let info = RGB(240, 240, 24).normal();
+	let debug = RGB(24, 192, 24).italic();
+	let header = RGB(255, 255, 255).bold();
+
+	println!("{}", err.paint("Test 1 "));
+	println!("{}", warn.paint("Test 2 "));
+	println!("{}", info.paint("Test 3 "));
+	println!("{}", debug.paint("Test 4 "));
+	println!();
+
+    println!("{}", header.paint("Guess the number!"));
+    println!("-----------------\n");
 
     print!("Enter your guess: ");
     io::stdout().flush().unwrap();
