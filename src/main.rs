c9c8ea0bd40fa9b9517c40a77b0c51923e04a519
Andrew Hickey <andrewhickey@geniesolutions.com.au>
2023-08-18 11:52:33 +1000

    RSLN-0002: Remove ansi test code

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
 use std::io::{self,Write};
-use ansi_term::Colour::RGB;
-//use ansi_term::Style;
+use rand::Rng;
 
 fn main() {
-	let err = RGB(248, 24, 24).normal();
-	let warn = RGB(230, 126, 34).normal();
-	let info = RGB(240, 240, 24).normal();
-	let debug = RGB(24, 192, 24).italic();
-	let header = RGB(255, 255, 255).bold();
 
-	println!("{}", err.paint("Test 1 "));
-	println!("{}", warn.paint("Test 2 "));
-	println!("{}", info.paint("Test 3 "));
-	println!("{}", debug.paint("Test 4 "));
-	println!();
-
-    println!("{}", header.paint("Guess the number!"));
+    println!("Guess the number!");
     println!("-----------------\n");
+
+    let my_num = rand::thread_rng().gen_range(1..=100);
+    eprintln!("DBG: my_num: {}", my_num);
 
     print!("Enter your guess: ");
     io::stdout().flush().unwrap();
@@ -30,18 +21,18 @@ fn main() {
 
     println!("You guessed {}.", guess_str);
     if guess_str.eq_ignore_ascii_case("quit") {
-        println!("{}", header.paint("OUT!"));
+        eprintln!("OUT!");
     }
     else
     {
-        print!("{}", err.paint("TODO: "));
-        println!("Actually compare the guess");
+        eprint!("TODO: ");
+        eprintln!("Actually compare the guess");
         
         let guess_num: u32 = match guess.parse() {
             Ok(num) => num,
             Err(_) => 9999,
         };
-        println!("{}", err.paint("Just what the fuck are you trying to do?"));
-        println!("{}{}", debug.paint("guess is "), guess_num);
+        eprintln!("Just what the fuck are you trying to do?");
+        eprintln!("guess is {}", guess_num);
     }
 }
