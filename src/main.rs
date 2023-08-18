487b617f80eac9ee7a1f7ad182909233c3cfa5c7
Andrew Hickey <andrewhickey@geniesolutions.com.au>
2023-08-18 11:58:20 +1000

    RSLN-0002: Remove unwrap and add error hanlding for std io

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,53 @@ fn main() {
 
     print!("Enter your guess: ");
     io::stdout().flush().unwrap();
+        // match io::stdin().read_line(&mut guess) {
+        //     Ok(s) => {
+        //         guess_now = match guess.strip_suffix("\n") {
+        //             Some(g) => {
+        //                 println!("got some");
+        //                 g.to_string()
+        //             },
+        //             None => {
+        //                 println!("Got none");
+        //                 guess
+        //             },
+        //         };
+        //         println!("Raw guess: {}, {}", guess_now, s);
+        //     },
+        //     Err(e) => { 
+        //         println!("Failed to read line {e:?}");
+        //         std::process::exit(1)
+        //     },
+        // }
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    match io::stdin().read_line(&mut guess) {
+        Ok(s) => {
+            eprintln!("Received {} bytes", s);
+            let guess_str = guess.trim().to_string();
 
-    let guess_str = guess.trim().to_string();
-
-    println!("You guessed {}.", guess_str);
-    if guess_str.eq_ignore_ascii_case("quit") {
-        eprintln!("OUT!");
-    }
-    else
-    {
-        eprint!("TODO: ");
-        eprintln!("Actually compare the guess");
-        
-        let guess_num: u32 = match guess.parse() {
-            Ok(num) => num,
-            Err(_) => 9999,
-        };
-        eprintln!("Just what the fuck are you trying to do?");
-        eprintln!("guess is {}", guess_num);
-    }
+            println!("You guessed {}.", guess_str);
+            if guess_str.eq_ignore_ascii_case("quit") {
+                eprintln!("OUT!");
+            }
+            else
+            {
+                eprint!("TODO: ");
+                eprintln!("Actually compare the guess");
+                
+                let guess_num: u32 = match guess.parse() {
+                    Ok(num) => num,
+                    Err(_) => 9999,
+                };
+                eprintln!("Just what the fuck are you trying to do?");
+                eprintln!("guess is {}", guess_num);
+            }
+        },
+        Err(e) => {
+            eprintln!("Failed to read input: {}", e);
+            std::process::exit(1)
+        }
+    }; // end match stdin
 }
