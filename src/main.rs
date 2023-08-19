a407182047fb91b4fedf67ea90d07a2083888157
NaMorham <namorham@gmail.com>
2023-08-19 13:07:19 +1000

    RSLN-0002: Remove number suffix test calls

--- src/main.rs
+++ src/main.rs
@@ -38,52 +38,6 @@ fn main() {
 
     let mut count = 0;
 
-    eprintln!("Test: {}", number_suffix(0));
-    eprintln!("Test: {}", number_suffix(1));
-    eprintln!("Test: {}", number_suffix(2));
-    eprintln!("Test: {}", number_suffix(3));
-    eprintln!("Test: {}", number_suffix(4));
-    eprintln!("Test: {}", number_suffix(10));
-    eprintln!("Test: {}", number_suffix(11));
-    eprintln!("Test: {}", number_suffix(12));
-    eprintln!("Test: {}", number_suffix(13));
-    eprintln!("Test: {}", number_suffix(14));
-    eprintln!("Test: {}", number_suffix(15));
-    eprintln!("Test: {}", number_suffix(16));
-    eprintln!("Test: {}", number_suffix(17));
-    eprintln!("Test: {}", number_suffix(18));
-    eprintln!("Test: {}", number_suffix(19));
-    eprintln!("Test: {}", number_suffix(20));
-    eprintln!("Test: {}", number_suffix(21));
-    eprintln!("Test: {}", number_suffix(22));
-    eprintln!("Test: {}", number_suffix(23));
-    eprintln!("Test: {}", number_suffix(24));
-    eprintln!("Test: {}", number_suffix(100));
-    eprintln!("Test: {}", number_suffix(101));
-    eprintln!("Test: {}", number_suffix(102));
-    eprintln!("Test: {}", number_suffix(103));
-    eprintln!("Test: {}", number_suffix(104));
-    eprintln!("Test: {}", number_suffix(110));
-    eprintln!("Test: {}", number_suffix(111));
-    eprintln!("Test: {}", number_suffix(112));
-    eprintln!("Test: {}", number_suffix(113));
-    eprintln!("Test: {}", number_suffix(114));
-    eprintln!("Test: {}", number_suffix(1000));
-    eprintln!("Test: {}", number_suffix(1001));
-    eprintln!("Test: {}", number_suffix(1002));
-    eprintln!("Test: {}", number_suffix(1003));
-    eprintln!("Test: {}", number_suffix(1004));
-    eprintln!("Test: {}", number_suffix(1010));
-    eprintln!("Test: {}", number_suffix(1011));
-    eprintln!("Test: {}", number_suffix(1012));
-    eprintln!("Test: {}", number_suffix(1013));
-    eprintln!("Test: {}", number_suffix(1014));
-    eprintln!("Test: {}", number_suffix(1050));
-    eprintln!("Test: {}", number_suffix(1051));
-    eprintln!("Test: {}", number_suffix(1052));
-    eprintln!("Test: {}", number_suffix(1053));
-    eprintln!("Test: {}", number_suffix(1054));
-
     #[cfg(debug_assertions)]
     eprintln!("DBG: my_num: {}", my_num);
 
